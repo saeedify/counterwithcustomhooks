78989bb9a8160ca2a861e9d6d2b0f1fcaf2203e4
Saeed Ghazi Naqvi <saeed.ghazi110@gmail.com>
2023-10-03 23:15:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCounter } from './components/useCounter'
 
 function App() {
+  const [number, increase, decrease, reset] = useCounter();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{number}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
